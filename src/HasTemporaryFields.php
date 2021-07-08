d530572182f24e28131b87b5237ca11e7800d76d
Rick Kuilman <rickkuilman@users.noreply.github.com>
2021-07-08 17:02:31 +0200

    Added compatibility for PHP 7.2.x
    Added compatibility for PHP 7.2.x

--- src/HasTemporaryFields.php
+++ src/HasTemporaryFields.php
@@ -8,6 +8,8 @@ trait HasTemporaryFields
 {
     protected static function fillFields(NovaRequest $request, $model, $fields)
     {
-        return parent::fillFields($request, $model, $fields->reject(fn ($field) => $field->meta['_temp'] ?? false));
+        return parent::fillFields($request, $model, $fields->reject(function ($field) {
+            return $field->meta['_temp'] ?? false;
+        }));
     }
 }
